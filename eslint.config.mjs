a632b991496731661db6ee19f404ac712e8ae701
Nick Lironi-Irvine <nick@iontech.nz>
2024-11-06 16:43:57 +1300

    Add prettier for consistent formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.strict,
-    ...tseslint.configs.stylistic,
-    {
-        ignores: ["**/*.js", "**/*.cjs"],
-    }
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ["**/*.js", "**/*.cjs"],
+  },
 );
