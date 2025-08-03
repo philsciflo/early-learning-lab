d5e2067b605faefeb8a589bac1e160c7c2d16cbd
JervidCheng <jehc177@aucklanduni.ac.nz>
2025-08-03 19:02:30 +1200

    Added new scoring display logic

--- games/apple-catcher/src/scenes/Level1Drop.ts
+++ games/apple-catcher/src/scenes/Level1Drop.ts
@@ -78,7 +78,7 @@ export class Level1Drop extends AbstractCatcherScene<Level1ScoringData> {
 
   private setupApple() {
     const appleX = this.rightTreeLeft + 100; 
-    const appleY = 415;
+    const appleY = this.treeY + 110;
 
     this.apple = this.physics.add
         .sprite(appleX, appleY, "apple")
@@ -111,7 +111,7 @@ private resetApple() {
     if (this.apple.body) {
         this.physics.world.disableBody(this.apple.body);
     }
-    this.apple.body.reset(this.rightTreeLeft + 100, 415); 
+    this.apple.body.reset(this.rightTreeLeft + 100, this.treeY + 110); 
     this.apple.setVisible(true);
     this.apple.setActive(true);
     this.apple.setInteractive(); // Re-enable dragging
